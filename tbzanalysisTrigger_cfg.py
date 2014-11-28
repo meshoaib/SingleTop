7c5013677dbf497da88be52e58e6204d92f03ea7
meshoaib <muhammad.shoaib@cern.ch>
2014-11-28 09:59:04 +0100

    cfg

--- tbzanalysisTrigger_cfg.py
+++ tbzanalysisTrigger_cfg.py
@@ -1,9 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
+#from SignalMC import *
+
 process = cms.Process("DEMO")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('RecoBTag/Configuration/RecoBTag_cff')
+process.load("Configuration.StandardSequences.Services_cff")
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+
 #JetMETCorrections
 process.load("JetMETCorrections.Type1MET.caloMETCorrections_cff")
 process.load("JetMETCorrections.Type1MET.correctionTermsCaloMet_cff")
@@ -15,9 +21,6 @@ process.load("JetMETCorrections.Type1MET.correctedMet_cff")
 #---------12-05-14-------
 #process.load("CommonTools.ParticleFlow.pfIsolation_cfg")
 #from CommonTools.ParticleFlow import pfIsolation_cfg
-#process.load("Configuration.StandardSequences.Services_cff")
-#process.load('Configuration.Geometry.GeometryIdeal_cff')
-#process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 #process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff') Configuration.Geometry.GeometryIdeal_cff
 #process.load("Configuration.StandardSequences.Reconstruction_cff")
 # HLT bit HLT_L1_BscMinBiasOR_BptxPlusORMinus
@@ -48,28 +51,22 @@ process.load("JetMETCorrections.Type1MET.correctedMet_cff")
 ########################################
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
 #process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
+
     #DATA2012A6
     #DATA2012A7
     #DATA2012A8
     #DATA2012A9
     #Data2101A
 
+    #SignalMCPat	
     #SignalMC
-    #TbZSignalMC
-    #TbZSignalMC
-    #Data2012A
-    #ZZ_MC
-    #WW_2012MC8TeV
-    #TTbarMC8TeV
-    #ZMC_2012
-    #WZ_2012MC
-    #WJETS_MC
-    #SignalMC
-#   'file:/afs/cern.ch/work/m/meshoaib/NewAnalysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/A27919CE-486A-E311-8EDD-002590D0B0C8.root'
+    #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/A27919CE-486A-E311-8EDD-002590D0B0C8.root'
+    #'file:/afs/cern.ch/work/m/meshoaib/NewAnalysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/A27919CE-486A-E311-8EDD-002590D0B0C8.root'
     #2012_EE
     #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/60CA2BDE-71DA-E111-9275-0024E86E8D4C.root' 
     #2012_DataSet
@@ -77,24 +74,35 @@ process.source = cms.Source("PoolSource",
     #DoublMu
     #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/F4FBA42A-A6CF-E111-BAAB-003048678A7E.root'
     #MuEG
-#    'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/0A47AE1F-BFD9-E111-8AB4-20CF3019DF0C.root'
-#WZ_MC
+    #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/0A47AE1F-BFD9-E111-8AB4-20CF3019DF0C.root'
+    #WZ_MC
     #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/38576F66-61DD-E111-A13B-002481E0DC4E.root'	
     ##'file:/afs/cern.ch/user/m/mwaqas/TBZ14/TBZMET/CMSSW_5_3_15/src/MyAnalysis/TbZ/001C0B68-536A-E311-B25F-002590D0B066.root'
     #'file:/afs/cern.ch/work/m/meshoaib/analysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/001C0B68-536A-E311-B25F-002590D0B066.root'
     #'file:/afs/cern.ch/work/m/meshoaib/Analysis_TBZ/CMSSW_5_3_7/src/FirstAnalysis/TBZAnalysis/A00B400A-B1CF-E111-BCEF-0026189438C2.root'
     #'file:/afs/cern.ch/work/m/meshoaib/Analysis_TBZ/CMSSW_5_3_7/src/FirstAnalysis/TBZAnalysis/441B933C-3846-E211-9928-00266CFFA768.root'
     #'file:/ehep/ehep_data/datasets/DoubleMu_2012RunA/13Jul2012-v1/AC00CD43-B9D2-E111-97FD-001A92971B20.root'    
-    #'file:/ehep/ehep_data/datasets/MC_2012/TT/0C8518DA-42F1-E111-BD2F-003048D3CB3C.root'
+    
+	#'file:/ehep/ehep_data/datasets/MC_2012/TT/0C8518DA-42F1-E111-BD2F-003048D3CB3C.root'
+	
 	# patTuple
-'file:/afs/cern.ch/work/m/meshoaib/NewAnalysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/singleTopSkim_TChannel.root'
+
+	#'file:/afs/cern.ch/work/m/meshoaib/NewAnalysis/CMSSW_5_3_15/src/MyAnalysis/TbZ/singleTopSkim_TChannel.root'
+	#'file:/afs/cern.ch/work/m/meshoaib/PatAnalysis/CMSSW_5_3_21/src/MyAnalysis/TbZ/singleTopSkim_TChannel_74_1_Ajb.root'
+	#"file:/afs/cern.ch/work/m/meshoaib/PatAnalysis/CMSSW_5_3_21/src/MyAnalysis/TbZ/singleTopSkim_TChannel.root"
+        
+	# "file:/afs/cern.ch/work/m/meshoaib/TupleSingleTop/CMSSW_5_3_11/src/TopQuarkAnalysis/SingleTop/test/singleTopSkim_TChannel.root"
+	"file:/afs/cern.ch/work/m/meshoaib/PatAnalysis/CMSSW_5_3_21/src/MyAnalysis/TbZ/singleTopSkim_TChannel.root"	
+	#input from EOS directly
+
+
     )
 )
 
 #process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')## To delete duplicate events, this included checkAllFilesOpened
 process.source.duplicateCheckMode = cms.untracked.string('checkAllFilesOpened')
 process.TFileService = cms.Service("TFileService",
-        fileName= cms.string("DoubleElectronB.root")
+        fileName= cms.string("ttbarMC_Pat.root")
 )
 
 print "before" 
@@ -179,18 +187,18 @@ process.topAna = cms.EDAnalyzer('TbZTopAnalyzer',
 # HLT_Mu17_Mu8_v16
 # HLT_Mu17_TkMu8_v9
 
-#MutriggerSelection   = cms.string('(HLT_Mu17_Mu8_* OR HLT_Mu17_TkMu8_*)'),
-#MuEGtriggerSelection = cms.string('(HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_* OR HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_*)'),
-#EtriggerSelection    = cms.string('(HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_*)'),
+MutriggerSelection   = cms.string('(HLT_Mu17_Mu8_* OR HLT_Mu17_TkMu8_*)'),
+MuEGtriggerSelection = cms.string('(HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_* OR HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_*)'),
+EtriggerSelection    = cms.string('(HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_*)'),
 
-#triggerConfiguration =  cms.PSet(
-#  hltResults = cms.InputTag('TriggerResults','','HLT'),
-#  l1tResults = cms.InputTag(''),
-#  daqPartitions = cms.uint32(1),
-#  l1tIgnoreMask = cms.bool( False ),
-#  l1techIgnorePrescales = cms.bool( False ),
-#  throw  = cms.bool( True )
-#),
+triggerConfiguration =  cms.PSet(
+  hltResults = cms.InputTag('TriggerResults','','HLT'),
+  l1tResults = cms.InputTag(''),
+  daqPartitions = cms.uint32(1),
+  l1tIgnoreMask = cms.bool( False ),
+  l1techIgnorePrescales = cms.bool( False ),
+  throw  = cms.bool( True )
+),
 
                                # electronsInputTag = cms.InputTag("myElectronProdLabel"),
                                # electronsInputTag = cms.InputTag("tightElectrons"),
@@ -215,10 +223,12 @@ process.topAna = cms.EDAnalyzer('TbZTopAnalyzer',
                                 
                                 metPtCut      = cms.double(25.)                                    ,
                                 vertexSrc     = cms.InputTag("offlinePrimaryVertices")             ,
-                                BtagEtaCut    = cms.double(2.4)                                    ,
-                                BtagPtCut     = cms.double(25)                                     ,
-                                BtagDiscrCut  = cms.double(0.679)                                  ,
-                                DPHiENue      = cms.double(1.0)                                    ,
+                                
+				BtagEtaCut    = cms.double(2.4)                                    ,
+                                BtagPtCut     = cms.double(30)                                     ,
+
+                                #BtagDiscrCut  = cms.double(0.679)                                  ,
+                               # DPHiENue      = cms.double(1.0)                                    ,
                                 DPHiMuNue     = cms.double(1.0)                                    ,
                                 MaxZMass      = cms.double(102.0)                                  ,
                                 MinZMAss      = cms.double(78.0)                                   ,
